417cc8d598ad43638a439e4cc4f139891166d4cb
Isaac Flores-Huerta <isaac@flores-huerta.com>
2025-09-04 21:34:14 -0400

    feat: add upload feature/route

---
+++ api/routers/upload.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, HTTPException, status, File, UploadFile
+import uuid
+
+router = APIRouter(prefix="/upload")
+
+
+@router.post("/upload", tags=["upload"])
+async def upload(file: UploadFile = File(...)):
+    file.filename = f"{uuid.uuid4()}.jpg"
+
+    try:
+        contents = await file.read()
+        with open(file.filename, "wb") as f:
+            f.write(contents)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong while uploading file"
+        )
+    finally:
+        file.file.close()
+
+    return {"filename": file.filename}
